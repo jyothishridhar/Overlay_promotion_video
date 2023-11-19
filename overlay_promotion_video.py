cf94a16f85895b94476e8fb063ad74851d316edf
Jyothi S <jyothi.s@genisys-group.com>
2023-11-19 17:09:39 +0530

    Update requirements.txt

--- overlay_promotion_video.py
+++ overlay_promotion_video.py
@@ -3,7 +3,7 @@ import openpyxl
 import streamlit as st
 import tempfile
 
-def detect_promotion_video(video_path):
+def detect_overlay(video_path):
     cap = cv2.VideoCapture(video_path)
 
     # Calculate histogram for the first frame
@@ -15,7 +15,7 @@ def detect_promotion_video(video_path):
     reference_hist = cv2.normalize(reference_hist, reference_hist).flatten()
 
     # Compare histograms of subsequent frames
-    promotion_frames = []
+    overlay_frames = []
 
     frame_count = 0
     while cap.isOpened():
@@ -31,17 +31,16 @@ def detect_promotion_video(video_path):
         correlation = cv2.compareHist(reference_hist, testing_hist, cv2.HISTCMP_CORREL)
         if correlation < 0.9:
             timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)  # Get timestamp in milliseconds
-            promotion_frames.append((timestamp, frame_count))
+            overlay_frames.append((timestamp, frame_count))
 
     cap.release()
 
-    return promotion_frames
+    return overlay_frames
 
-
-def generate_excel_report(reference_promotion_frames, testing_promotion_frames, report_path):
+def generate_overlay_excel_report(reference_overlay_frames, testing_overlay_frames, report_path):
     wb = openpyxl.Workbook()
     sheet = wb.active
-    sheet.title = "Promotion Frames"
+    sheet.title = "Overlay Frames"
 
     sheet['A1'] = "Reference Timestamp"
     sheet['B1'] = "Reference Frame Number"
@@ -50,18 +49,18 @@ def generate_excel_report(reference_promotion_frames, testing_promotion_frames, 
     sheet['E1'] = "Timestamp Difference"
     sheet['F1'] = "Frame Number Difference"
 
-    max_length = max(len(reference_promotion_frames), len(testing_promotion_frames))
+    max_length = max(len(reference_overlay_frames), len(testing_overlay_frames))
 
     for row_num in range(2, max_length + 2):
-        if row_num <= len(reference_promotion_frames):
-            ref_timestamp, ref_frame_num = reference_promotion_frames[row_num - 2]
+        if row_num <= len(reference_overlay_frames):
+            ref_timestamp, ref_frame_num = reference_overlay_frames[row_num - 2]
             sheet.cell(row=row_num, column=1, value=ref_timestamp)
             sheet.cell(row=row_num, column=2, value=ref_frame_num)
-        if row_num <= len(testing_promotion_frames):
-            test_timestamp, test_frame_num = testing_promotion_frames[row_num - 2]
+        if row_num <= len(testing_overlay_frames):
+            test_timestamp, test_frame_num = testing_overlay_frames[row_num - 2]
             sheet.cell(row=row_num, column=3, value=test_timestamp)
             sheet.cell(row=row_num, column=4, value=test_frame_num)
-            if row_num <= len(reference_promotion_frames):
+            if row_num <= len(reference_overlay_frames):
                 timestamp_diff = test_timestamp - ref_timestamp
                 frame_num_diff = test_frame_num - ref_frame_num
                 sheet.cell(row=row_num, column=5, value=timestamp_diff)
@@ -85,10 +84,10 @@ if st.button("Run Overlay Detection"):
             reference_path = ref_temp.name
             testing_path = test_temp.name
 
-        reference_promotion_frames = detect_promotion_video(reference_path)
-        testing_promotion_frames = detect_promotion_video(testing_path)
+        reference_overlay_frames = detect_overlay(reference_path)
+        testing_overlay_frames = detect_overlay(testing_path)
 
-        generate_excel_report(reference_promotion_frames, testing_promotion_frames, report_path)
+        generate_overlay_excel_report(reference_overlay_frames, testing_overlay_frames, report_path)
 
         st.success("Overlay detection completed.")
     else:
