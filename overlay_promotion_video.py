03f3badfd38dcdf0a08bb659ffd45c39324f27a1
Jyothi S <jyothi.s@genisys-group.com>
2023-11-30 12:22:59 +0530

    Update requirements.txt

--- overlay_promotion_video.py
+++ overlay_promotion_video.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 import streamlit as st
 import tempfile
@@ -11,53 +10,41 @@ def download_video(url):
     return io.BytesIO(response.content)
 
 def detect_overlay(video_content):
-    # Create a temporary file
-    with tempfile.TemporaryFile(suffix=".mp4") as temp_file:
-        temp_file.write(video_content.getvalue())
-        temp_file_path = temp_file.name
+    cap = cv2.VideoCapture(video_content)
 
-        # Open the video file using VideoCapture
-        cap = cv2.VideoCapture(temp_file_path)
+    # Calculate histogram for the first frame
+    ret, reference_frame = cap.read()
+    if not ret:
+        return []
 
-        try:
-            # Calculate histogram for the first frame
-            ret, reference_frame = cap.read()
-            if not ret:
-                return []
+    reference_hist = cv2.calcHist([reference_frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    reference_hist = cv2.normalize(reference_hist, reference_hist).flatten()
 
-            reference_hist = cv2.calcHist([reference_frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-            reference_hist = cv2.normalize(reference_hist, reference_hist).flatten()
+    # Compare histograms of subsequent frames
+    overlay_frames = []
 
-            # Compare histograms of subsequent frames
-            overlay_frames = []
+    frame_count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-            frame_count = 0
-            while cap.isOpened():
-                ret, frame = cap.read()
-                if not ret:
-                    break
+        frame_count += 1
+        testing_hist = cv2.calcHist([frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+        testing_hist = cv2.normalize(testing_hist, testing_hist).flatten()
 
-                frame_count += 1
-                testing_hist = cv2.calcHist([frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-                testing_hist = cv2.normalize(testing_hist, testing_hist).flatten()
+        # Compare histograms using correlation
+        correlation = cv2.compareHist(reference_hist, testing_hist, cv2.HISTCMP_CORREL)
+        if correlation < 0.2:
+            # Get the timestamp of the current frame
+            timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+            overlay_frames.append((timestamp, cap.get(cv2.CAP_PROP_POS_FRAMES)))
 
-                # Compare histograms using correlation
-                correlation = cv2.compareHist(reference_hist, testing_hist, cv2.HISTCMP_CORREL)
-                if correlation < 0.2:
-                    # Get the timestamp of the current frame
-                    timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-                    overlay_frames.append((timestamp, cap.get(cv2.CAP_PROP_POS_FRAMES)))
+        print(f"Correlation: {correlation}")
 
-                print(f"Correlation: {correlation}")
-
-        finally:
-            cap.release()
+    cap.release()
 
     return overlay_frames
-
-
-
-# ... (rest of the code remains unchanged)
 
 def generate_overlay_report_df(reference_overlay_frames, testing_overlay_frames):
     max_length = max(len(reference_overlay_frames), len(testing_overlay_frames))
@@ -115,7 +102,6 @@ def generate_overlay_reports(reference_overlay_frames, testing_overlay_frames):
 st.title("Overlay Detection Demo")
 
 # Git LFS URLs for the videos
-# Corrected Git LFS URLs for the videos
 reference_video_url = "https://github.com/jyothishridhar/Overlay_promotion_video/raw/main/concat_video_1.mp4"
 testing_video_url = "https://github.com/jyothishridhar/Overlay_promotion_video/raw/main/concat_video_2.mp4"
 
